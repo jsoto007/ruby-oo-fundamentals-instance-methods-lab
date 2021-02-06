cdcf944a55d01ae7800babc2b59ba7e03fa30861
Jose Soto <jsoto7087@gmail.com>
2021-02-06 16:12:01 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+
+class Dog
+
+def bark
+    puts "Woof!"
+    @bark
+end
+
+def sit
+    puts "The Dog is sitting"
+    @sit
+end 
+
+end 
+
---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+
+
+class Person
+    
+    def talk
+        puts "Hello World!"
+        @talk
+    end
+
+    def walk
+        puts "The Person is walking"
+        @walk
+    end
+
+end 
+
+
+
+
+
+
