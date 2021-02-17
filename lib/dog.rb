a373bbd6a422af30723b80b1b011efa7910beded
Jose Soto <jsoto7087@gmail.com>
2021-02-17 14:09:02 -0500

    commit tree 06e1bd90ab534b1cabce8bb59905cd5e08be1dce

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,42 @@
 #!/usr/bin/env ruby
 
 
-class Dog
+# class Dog
 
-def bark
-    puts "Woof!"
-    @bark
-end
+# def bark
+#     puts "Woof!"
+#     @bark
+# end
 
-def sit
-    puts "The Dog is sitting"
-    @sit
+# def sit
+#     puts "The Dog is sitting"
+#     @sit
+# end 
+
+# end 
+
+
+
+
+
+
+
+
+class Dog 
+    attr_accessor :bark
+
+    def bark
+        @bark 
+        puts "Woof!"
+    end 
+
+    def sit 
+        @sit 
+        puts "The Dog is sitting"
+    end 
+
 end 
 
-end 
+
+
 
